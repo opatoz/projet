747e4dae57d045496e081f8ae72feb7b7fd60478
Justin <Justin@10.172.103.244>
2025-03-26 14:54:55 +0100

    premier commit

---
+++ src/main/java/com/mycompany/projets2/ProjetS2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author Justin
+ */
+public class ProjetS2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
