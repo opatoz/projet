5e2d4d3d213b770a8e52e3cb35baf3b59a7346ff
Justin <Justin@10.172.103.244>
2025-03-26 15:51:24 +0100

    machine+poste

---
+++ src/main/java/com/mycompany/projets2/Poste.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author Justin
+ */
+public class Poste {
+    private int refposte;
+    private String dposte;
+    
+}
