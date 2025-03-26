b515a0ad3d8c92c10df92dcbbfb9ec30b7899d99
Justin <Justin@10.172.103.244>
2025-03-26 15:52:17 +0100

    machine+poste

---
+++ src/main/java/com/mycompany/projets2/Machine.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author Justin
+ */
+public class Machine extends Equipement {
+    private int ref;
+    private String dmachine;
+    private float abscisse;
+    private float ordonnée;
+    private float coût;
+    private float durée;
+}
