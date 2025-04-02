89cbeb7ec119c42528f4b060c26152f0717eae5a
Justin <Justin@10.172.103.244>
2025-04-02 09:48:24 +0200

    machine get + set

--- src/main/java/com/mycompany/projets2/Machine.java
+++ src/main/java/com/mycompany/projets2/Machine.java
@@ -9,10 +9,69 @@ package com.mycompany.projets2;
  * @author Justin
  */
 public class Machine extends Equipement {
-    private int ref;
+    private int refmachine;
     private String dmachine;
-    private float abscisse;
-    private float ordonnée;
+    private float x;
+    private float y;
     private float coût;
     private float durée;
+    private String type;
+
+    public int getRefmachine() {
+        return refmachine;
+    }
+
+    public String getDmachine() {
+        return dmachine;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getCoût() {
+        return coût;
+    }
+
+    public float getDurée() {
+        return durée;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setRefmachine(int refmachine) {
+        this.refmachine = refmachine;
+    }
+
+    public void setDmachine(String dmachine) {
+        this.dmachine = dmachine;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setCoût(float coût) {
+        this.coût = coût;
+    }
+
+    public void setDurée(float durée) {
+        this.durée = durée;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
 }
