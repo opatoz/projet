96d1e83ff1229c2d004dde2f4787dc276704251b
Elève <Elve@172.17.20.225>
2025-04-02 09:09:23 +0200

    classe equipement + get et set

---
+++ src/main/java/com/mycompany/projets2/Equipement.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author Elève
+ */
+public class Equipement {
+    private String identifiant;
+
+    public String getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(String identifiant) {
+        this.identifiant = identifiant;
+    }
+    
+}
