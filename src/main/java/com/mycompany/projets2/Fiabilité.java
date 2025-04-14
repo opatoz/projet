929a10fadd3e1605cacb781e103a25ac64f59a4d
Elève <Elve@172.17.20.225>
2025-04-14 20:40:19 +0200

    classe Fiabilité

---
+++ src/main/java/com/mycompany/projets2/Fiabilité.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projets2;
+
+/**
+ *
+ * @author Elève
+ */
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Fiabilité {
+    
+    private Map<String, Long> tempsArret = new HashMap<>(); // pour stocker les temps d'arrêt totals de chaque machine
+    private Map<String, Evenement> arretsEnCours = new HashMap<>(); // pour mémoriser l'evenement de type A et l'utiliser pour calculer le temps d'arret d'une machine
+
+    public void analyser(List<Evenement> ev) {
+        for (Evenement e : ev) {
+            String machine = e.getMachine();// récupère le nom de la machine et le type d'evenement 
+            char type = e.getTypeEv(); //.toUpperCase(); que si de type String
+
+            if (type=='A') {
+                arretsEnCours.put(machine, e); // stocke l'ev avec sa machine associé si c'est un arrêt
+            } else if (type=='D') {
+                Evenement debut = arretsEnCours.get(machine); // si ev est un redémarrage de la mchine on cherche le dernier arrêt de cette machine
+                if (debut != null) { // vérifie que cet évenement existe bien (normalement oui)
+                    long duree = calculerDuree(debut.getDate(), debut.getHeure(), e.getDate(), e.getHeure());
+                    tempsArret.put(machine, tempsArret.getOrDefault(machine, 0L) + duree); //ajoute la durée d'arrêt dans le temps totale d'arrêt de la machine
+                    arretsEnCours.remove(machine); // on retire l'arrêt de la machine une fois traité de la "mémoire" pour que qu'en on recroise "D" on sache avec quel "A" faire le calcule (avec le seul stocké = le dernier rencontré)
+                }
+                else { 
+                    System.out.println("Il manque l'évenement d'arrêt pour la machine "+machine);
+                }
+            }
+        }
+    }
+
+    private long calculerDuree(String dateDebut, String heureDebut, String dateFin, String heureFin) {
+        
+        //crée un DateTimeFormatter pour pouvoir écrire les date et les heures sous un format spécifié que java peut interpréter/manipuler 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        //converti date et heure de début et de fin en LocalDateTime, le format que Java peut manipuler 
+        LocalDateTime debut = LocalDateTime.parse(dateDebut + " " + heureDebut, formatter);
+        LocalDateTime fin = LocalDateTime.parse(dateFin + " " + heureFin, formatter);
+        //calcul la durée entre les deux instants
+        Duration duree = Duration.between(debut, fin);
+        return duree.toMinutes();
+    }
+
+    public List<Map.Entry<String, Long>> TrieMachine() {
+        //crée une liste triable qui va contenir des parie clé-valeurs de la map TempsArret
+        List<Map.Entry<String, Long>> listeTemps = new ArrayList<>(tempsArret.entrySet());
+        //effectue le trie en fonction de la valeur = temps d'arrêt
+        listeTemps.sort(Map.Entry.comparingByValue());
+        
+        return listeTemps;
+    }
+    
+    public void afficherClassement(List<Map.Entry<String, Long>> listeTemps){
+        System.out.println("Machines les plus fiables (moins de pannes) :");
+        for (Map.Entry<String, Long> paire : listeTemps) {
+            //converti
+            long heures = paire.getValue()/60;
+            long minutes = paire.getValue()%60;
+                    
+            System.out.println("Machine" + paire.getKey() + ":" + heures + "h"+minutes +"min");
+        }
+    }
+
+    //pas besoin mais au cas ou :
+    public Map<String, Long> getTempsArret() {
+        return tempsArret;
+    }
+
+    public void setTempsArret(Map<String, Long> tempsArret) {
+        this.tempsArret = tempsArret;
+    }
+
+    public Map<String, Evenement> getArretsEnCours() {
+        return arretsEnCours;
+    }
+
+    public void setArretsEnCours(Map<String, Evenement> arretsEnCours) {
+        this.arretsEnCours = arretsEnCours;
+    }
+    
+}
