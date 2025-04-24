12001c8b887317567b4c8299a39bac600ebaf473
Elève <Elve@172.17.20.225>
2025-04-24 23:00:40 +0200

    constru fiabilité

--- src/main/java/com/mycompany/projets2/Fiabilité.java
+++ src/main/java/com/mycompany/projets2/Fiabilité.java
@@ -17,6 +17,10 @@ public class Fiabilité {
     private Map<String, Long> tempsArret = new HashMap<>(); // pour stocker les temps d'arrêt totals de chaque machine
     private Map<String, Evenement> arretsEnCours = new HashMap<>(); // pour mémoriser l'evenement de type A et l'utiliser pour calculer le temps d'arret d'une machine
 
+    public Fiabilité (Map<String, Long> tempsArret, Map<String, Evenement> arretsEnCours ){
+        this.tempsArret = tempsArret;
+        this.arretsEnCours = arretsEnCours;
+}
     public void analyser(List<Evenement> ev) {
         for (Evenement e : ev) {
             String machine = e.getMachine();// récupère le nom de la machine et le type d'evenement 
